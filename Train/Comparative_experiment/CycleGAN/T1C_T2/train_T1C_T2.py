94659854ce4719d8ce09308fc7d635e034d6a590
yi-bobo <1757693615@qq.com>
2025-07-15 11:12:09 +0800

    下游实验

--- Train/Comparative_experiment/CycleGAN/T1C_T2/train_T1C_T2.py
+++ Train/Comparative_experiment/CycleGAN/T1C_T2/train_T1C_T2.py
@@ -247,7 +247,7 @@ def main():
         save_loss_csv(opt.path.train_avg_loss_csv_path, epoch,  ['epoch'] + list(avg_loss_dict.keys()), {'epoch': epoch, **avg_loss_dict})
 
         # 学习率调度与模型保存
-        if lr_G > opt.optom_G.lr_min:
+        if lr_G > opt.optim_G.lr_min:
             model.scheduler_G.step()
             model.scheduler_D.step()
         save_model(model_components, epoch, opt.path.checkpoint_path_dir, file_name=f"latest.pth")
